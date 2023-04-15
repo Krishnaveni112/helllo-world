5756b28c3b3011c666a35bab1d1c86c7c61624d6
Krishnaveni Sidduri <siddurikrishnaveni@gmail.com>
2023-04-14 23:56:42 -0400

    intellij subtraction

--- src/main/java/org/example/MathLabExample.java
+++ src/main/java/org/example/MathLabExample.java
@@ -7,7 +7,9 @@ public class MathLabExample {
         int a=12;
         int b=14;
         int sum=a+b;
+        int subtraction=b-a;
         System.out.println(sum);
+        System.out.println(subtraction);
     }
 
 
