5379d853487ed29db52f48d5af365d2a20bf1689
Krishnaveni112 <129026601+Krishnaveni112@users.noreply.github.com>
2023-04-14 22:38:26 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         for(int i=0;i<10;i++){
             System.out.println("Hello world!");
+            System.out.println("Hi Sriram");
         }
     }
 }
