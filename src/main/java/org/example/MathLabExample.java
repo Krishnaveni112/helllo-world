a8c3baa7e45379cb0343aba1d00d1bb9f7e68f3f
Krishnaveni Sidduri <siddurikrishnaveni@gmail.com>
2023-04-14 23:02:18 -0400

    Adding sum method

---
+++ src/main/java/org/example/MathLabExample.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class MathLabExample {
+
+
+    public static void main(String[] args) {
+        int a=12;
+        int b=14;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+
+
+}
