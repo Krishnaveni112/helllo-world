1d30bbf260748a71a97d334efdd7103b0d821e55
Krishnaveni Sidduri <siddurikrishnaveni@gmail.com>
2024-01-28 18:27:54 -0500

    Commiting myProject changes

--- src/main/java/org/example/MyFirstProject.java
+++ src/main/java/org/example/MyFirstProject.java
@@ -3,5 +3,6 @@ package org.example;
 public class MyFirstProject {
     public static void main(String[] args){
         System.out.println("Helloworld");
+        System.out.println("MyFirstProject");
     }
 }
